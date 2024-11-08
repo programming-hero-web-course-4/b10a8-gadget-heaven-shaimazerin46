74950bc9621e505cec301fd20879b51cef80a225
unknown <shaimazerin46@gmail.com>
2024-11-08 11:28:43 +0600

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,40 @@
 
 const Footer = () => {
     return (
-        <div>
-            <div>
-                
+        <div className="mt-20 p-10">
+            <hr />
+            <div className="w-1/2 mx-auto text-center mt-10">
+                <h3 className="mb-3 font-bold text-3xl">Gadget Heaven</h3>
+                <p>Leading the way in cutting-edge technology and innovation.</p>
+            </div>
+            <div className="flex justify-between mt-10 ">
+                <div className="text-center">
+                    <h3 className="font-bold text-xl mb-3">Services</h3>
+                    <ul className="text-light space-y-3">
+                        <li>Product Support</li>
+                        <li>Order Tracking</li>
+                        <li>Shipping & Delivery</li>
+                        <li>Returns</li>
+                    </ul>
+                </div>
+                <div className="text-center">
+                <h3 className="font-bold text-xl mb-3">Company</h3>
+                    <ul className="text-light space-y-3">
+                    <li>About Us</li>
+                        <li>Careers</li>
+                        <li>Contact</li>
+                       
+                    </ul>
+                </div>
+                <div className="text-center">
+                <h3 className="font-bold text-xl mb-3">Legal</h3>
+                    <ul className="text-light space-y-3">
+                        <li>Terms of Service</li>
+                        <li>Privacy Policy</li>
+                        <li>Cookie Policy</li>
+                        
+                    </ul>
+                </div>
             </div>
         </div>
     );
--- src/components/Roots/Roots.jsx
+++ src/components/Roots/Roots.jsx
@@ -22,6 +22,7 @@ const Roots = () => {
                 
             </GadgetsContext.Provider>
             
+            
             <Footer></Footer>
            
             
